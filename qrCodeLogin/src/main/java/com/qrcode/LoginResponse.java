439f7a9f481641a735ade1aaf05be1a74e36dc9a
1843080373@qq.com <1843080373@qq.com>
2019-04-15 16:10:01 +0800

    java扫码登陆

---
+++ qrCodeLogin/src/main/java/com/qrcode/LoginResponse.java
@@ -0,0 +1,31 @@
+package com.qrcode;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 登录信息承载类
+ *
+ */
+public class LoginResponse {
+
+    public CountDownLatch latch;
+
+    public String user;
+
+	public CountDownLatch getLatch() {
+		return latch;
+	}
+
+	public void setLatch(CountDownLatch latch) {
+		this.latch = latch;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+    
+}
